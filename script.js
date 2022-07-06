417bb90a73175a407f82ccd7699746513ddd0fb2
Junjifan <52554368+dehumaniz@users.noreply.github.com>
2022-07-05 21:53:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function escolha(){
+        if (socorr1) {
+        alert("pois é sabemos")
+    } else if(socorr2){
+        alert("POIS É, REALMENTE SABEMOS")
+    }else if(socorr3){
+        alert("sindicato, realmente")
+    }
+}
